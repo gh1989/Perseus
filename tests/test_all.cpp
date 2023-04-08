e0f0d7570c9d1de0305b4e36e9d56237a6558f7f
Gregg <gregg.hutchence@gmail.com>
2023-04-08 20:20:25 +0100

    test_all

--- tests/test_all.cpp
+++ tests/test_all.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_all.hpp>
 #include <iostream>
 #include "../src/bitboard.h"
+#include "../src/state.h"
+#include "../src/string_transforms.h"
 
 #define CATCH_CONFIG_MAIN
 
@@ -163,6 +165,34 @@ TEST_CASE("nLSB returns correct result for non-zero bitboards", "[bitboard]") {
             count++;
         }
         REQUIRE(count == 64);
+    }
+}
+
+TEST_CASE("FEN to state conversion", "[state]")
+{
+    SECTION("Initial state")
+    {
+        std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        /*
+        State state = StateFromFen(fen);
+        REQUIRE(state.bbs[0] == 0x0000000000000010ULL);  // white knights
+        REQUIRE(state.bbs[1] == 0x0000000000000024ULL);  // white bishops
+        REQUIRE(state.bbs[2] == 0x0000000000000081ULL);  // white rooks
+        REQUIRE(state.bbs[3] == 0x0000000000000008ULL);  // white queen
+        REQUIRE(state.bbs[4] == 0x0000000000000010ULL);  // white king
+        REQUIRE(state.bbs[5] == 0x000000000000FF00ULL);  // white pawns
+        REQUIRE(state.bbs[6] == 0x0000000000001000ULL);  // black knights
+        REQUIRE(state.bbs[7] == 0x0000000000000042ULL);  // black bishops
+        REQUIRE(state.bbs[8] == 0x8100000000000000ULL);  // black rooks
+        REQUIRE(state.bbs[9] == 0x0800000000000000ULL);  // black queen
+        REQUIRE(state.bbs[10] == 0x1000000000000000ULL); // black king
+        REQUIRE(state.bbs[11] == 0x00FF000000000000ULL); // black pawns
+        REQUIRE(state.castle == 15);  // all castling rights
+        REQUIRE(state.turn == 0);     // white to move
+        REQUIRE(state.bbs[12] == 0);  // no en passant square
+        REQUIRE(state.c50 == 0);      // 50-move rule not applicable
+        REQUIRE(state.plies == 0);    // first move
+        */
     }
 }
 
