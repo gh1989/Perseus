32caa49dbe262070bac05a25d65cfd8bce4a9be6
Gregg <gregg.hutchence@gmail.com>
2023-05-08 00:28:02 +0100

    Change to library try to expose in pybind

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <numeric>
+#include <torch/torch.h>
+#include <torch/nn/functional.h>
 
 #include "network.h"
 #include "string_transforms.h"
@@ -10,5 +12,12 @@ int main()
     ChessInputLayerImpl network;
     State state = StateFromFen();
     std::cout << network.forward(state);
+    SimpleChessNet net;
+    auto input = network.forward(state);
+    auto netOutput = net.forward( input );
+    auto value = std::get<0>(netOutput);
+    auto policy = std::get<1>(netOutput);
+    //std::cout << value.item<float>() << std::endl;
+    std::cout << policy.squeeze() << std::endl;
     return 0;
 }
