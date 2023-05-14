5436b0ff5e52879099f0ebbd2f285b23e73319d8
Gregg <gregg.hutchence@gmail.com>
2023-05-14 02:23:41 +0100

    library

--- src/py_chessnetwork.cpp
+++
@@ -1,9 +0,0 @@
-#include "py_chessnetwork.h"
-
-/*
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    pybind11::class_<ChessNetworkImpl>(m, "ChessNetwork")
-        .def(pybind11::init<>())
-        .def("forward", &ChessNetworkImpl::forward);
-}
-*/
--- src/py_chessnetwork.h
+++
@@ -1,8 +0,0 @@
-#ifndef PY_CHESSNETWORK_H
-#define PY_CHESSNETWORK_H
-
-//#include <pybind11/pybind11.h>
-#include <torch/torch.h>
-#include "network.h"
-
-#endif
